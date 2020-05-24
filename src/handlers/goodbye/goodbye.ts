35ff8cce60c42ff1fb5660148f9825c48c5dfbe6
Anish Kargaonkar <anish@rocketium.com>
2020-05-24 20:59:33 +0530

    add goodbye handler

---
+++ src/handlers/goodbye/goodbye.ts
@@ -0,0 +1,6 @@
+import { APIGatewayProxyResult } from 'aws-lambda';
+
+export const handler = async (): Promise<APIGatewayProxyResult> => {
+	
+  return { body: JSON.stringify({ message: 'goodbye' }), statusCode: 200 };
+};
